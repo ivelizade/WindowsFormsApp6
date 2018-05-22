fbefecd1c034fef1485a701c06015254f96012b2
Velizadeh Ibrahim <ivelizade@outlook.com>
2018-05-22 21:58:24 +0400

    Add project files.

---
+++ WindowsFormsApp6/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string EMail { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
